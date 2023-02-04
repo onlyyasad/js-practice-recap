8e36d40e17f5bb08733c8bbea290fb7e8bd7ee98
onlyyasad <onlyyasad@gmail.com>
2023-02-05 01:37:51 +0600

    average added

---
+++ average.js
@@ -0,0 +1,14 @@
+
+
+function averageMaker(arr){
+    let total = 0;
+    for (number of arr){
+        total += number;
+    }
+    let average = total / arr.length;
+    return average;
+}
+
+const numbers = [40, 70, 25, 35, 30];
+const theAverage = averageMaker(numbers);
+console.log(theAverage)
